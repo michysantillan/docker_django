fee69a8885703b3da5f101c61b6b91bd501ee541
michysantillan <michysantillan@gmail.com>
2020-04-07 16:18:39 -0300

    se cambio el mensaje

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hola mundo desde un django dockerizado con postgres nginx y gunicorn,sincronizado con git")
